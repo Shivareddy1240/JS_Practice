837f9e4f6ced0b2831d447f9744cb725a8e9b598
Shiva Reddy M <shivamudhireddy@gmail.com>
2024-12-23 14:21:18 +0530

    first commit

---
+++ Variables.js
@@ -0,0 +1,39 @@
+//Variable name can contain letters,numbers,_,$
+//Start with letter,_,$
+//case-sensitive
+
+let name; //Undefined
+console.log(name);
+
+let firstname = "Shiva Reddy"
+console.log(firstname);
+
+//Declaring and Assignments
+let id,address,designation;
+id= 1;
+address = "Hyderabad";
+designation = "Manager";
+firstname = "Shiva"; // Updating
+console.log(firstname,id,address,designation);
+
+//case-sensitive - below two are seperate variables
+let myname = "Shiva";
+console.log(myname);
+let Myname = "Shiva";
+console.log(Myname);
+
+//must start with letter,_ or $
+let s1 = "season1";
+let _season = "season2";
+let $season = "season3";
+// let 1season = "Season"; Not allowed
+console.log(s1,_season,$season);
+
+//Good practice of variable names
+let firstName = "Shiva"
+let first_name = "Shiva"
+
+
+
+
+
